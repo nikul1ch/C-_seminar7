c55861fe07ea129d06d22500e57692b606edd978
nikul1ch <nikulin@netkam.ru>
2022-09-09 13:45:09 +0300

    Программ, которая задаёт двумерный массив размером m*n, заполненный случайными вещественными числами.

--- seminar7.1/Program.cs
+++ seminar7.1/Program.cs
@@ -26,25 +26,20 @@ void PrintMatrix(double[,] array)
 
 }
 
-// int InputVariable(int var)                           Хотел вынести считывание из консоли в метод
-// {                                                    В консоли пытался вывести имя переменной, чтобы пользователь понимал какую переменную он заполняет
-//     Console.Write($"Введите {"var"}: ");             Но ничего не вышло, решил оставить ввод консоли без метода)  
-//     return Convert.ToInt32(Console.ReadLine());      При проверке предыдущих ДЗ вы писали что лучше вынести в метод (3 и 4 дз), но чем лучше не понял
-// }
+int InputVariable()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-//int rowCount = InputVariable(rowCount = 0);           
-//int columnCount = InputVariable(columnCount = 0);
-//double min = InputVariable(min = 0);
-//double max = InputVariable(max = 0);
 
 Console.Write($"Введите количество строк: ");
-int rowCount = Convert.ToInt32(Console.ReadLine());
+int rowCount = InputVariable();
 Console.Write($"Введите количество столбцов: ");
-int columnCount = Convert.ToInt32(Console.ReadLine());
+int columnCount = InputVariable();
 Console.Write($"Введите минимальную грацицу диапазона чисел: ");
-int min = Convert.ToInt32(Console.ReadLine());
+int min = InputVariable();
 Console.Write($"Введите максимальную грацицу диапазона чисел: ");
-int max = Convert.ToInt32(Console.ReadLine());
+int max = InputVariable();
 
 double[,] array = CreateMatrix(rowCount, columnCount, min, max);
 PrintMatrix(array);
--- seminar7.2/Program.cs
+++ seminar7.2/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double[,] CreateMatrix(int rowCount, int columnCount)
+{
+    double[,] array = new double[rowCount, columnCount];
+    for (int row = 0; row < rowCount; row++)
+    {
+        for (int column = 0; column < columnCount; column++)
+        {
+            array[row, column] = Math.Round(new Random().NextDouble() * 10, 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(double[,] array)
+{
+    for (int row = 0; row < array.GetLength(0); row++)
+    {
+        Console.Write($"|\t");
+        for (int column = 0; column < array.GetLength(1); column++)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.Write("|");
+        Console.WriteLine();
+    }
+
+}
+
+void ReturnValue(int row, int column, double[,] array)
+{
+    if (row < 0 || row > array.GetLength(0) - 1 || column < 0 || column > array.GetLength(1) - 1) Console.Write($"Элемента с индексом [{row},{column}] в массиве нет!");
+    else Console.Write($"Элемент с индексом [{row},{column}] = {array[row, column]}");
+}
+
+int InputVariable()
+{
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write($"Введите количество строк: ");
+int rowCount = InputVariable();
+Console.Write($"Введите количество столбцов: ");
+int columnCount = InputVariable();
+Console.Write($"Введите номер строки искомого значения: ");
+int rowNumber = InputVariable();
+Console.Write($"Введите номер столбца искомого значения: ");
+int columnNumber = InputVariable();
+
+double[,] array = CreateMatrix(rowCount, columnCount);
+PrintMatrix(array);
+Console.WriteLine();
+ReturnValue(rowNumber, columnNumber, array);
